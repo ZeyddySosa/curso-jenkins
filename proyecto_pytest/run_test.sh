b4bf60a801dba7061e905dfae9b1b709e825034f
Zeyddy Sosa <zeyddy.sosa@ceo.do>
2026-06-23 13:31:55 -0400

    Correccion de entorno virtual

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
 echo "Activando entorno virtual"
+if [!"-d venv" ]; then
+    python3 -m venv venv
+fi
+
 source venv/bin/activate
 
 echo "Instalando dependencias"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Ejecutando pruebas con pytest"
